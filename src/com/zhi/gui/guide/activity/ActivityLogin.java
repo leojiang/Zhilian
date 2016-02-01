f4f8a866adfd2aceabc89b79d74b94c1db9108e0
leojiang <jiangyanghaics@163.com>
2016-02-01 21:34:18 +0800

    constructed login ui without ajust positions of subviews

--- src/com/zhi/gui/guide/activity/ActivityLogin.java
+++ src/com/zhi/gui/guide/activity/ActivityLogin.java
@@ -1,7 +1,31 @@
 package com.zhi.gui.guide.activity;
 
+
+import com.zhi.gui.guide.R;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class ActivityLogin extends Activity {
+    private TextView mTextLookAround;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mTextLookAround = (TextView) findViewById(R.id.text_lookaround);
+        mTextLookAround.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                startActivity(new Intent(getApplicationContext(), ActivityMainTab.class));
+                finish();
+            }
+        });
+    }
 }
