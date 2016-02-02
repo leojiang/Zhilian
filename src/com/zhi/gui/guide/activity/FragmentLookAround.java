82292382a90a1690b425cb27dcdc88ccc3317957
leojiang <jiangyanghaics@163.com>
2016-02-02 14:58:53 +0800

    added adapter for lookaround activity

--- src/com/zhi/gui/guide/activity/FragmentLookAround.java
+++ src/com/zhi/gui/guide/activity/FragmentLookAround.java
@@ -6,12 +6,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class FragmentLookAround extends Fragment {
+
+    private ListView mListIndustry;
+    private ListView mListRanking;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_lookaround, null);
+        View root = inflater.inflate(R.layout.fragment_lookaround, null);
+        mListIndustry = (ListView) root.findViewById(R.id.list_industry);
+        mListRanking = (ListView) root.findViewById(R.id.list_ranking);
+        initView();
+        return root;
+    }
+
+    private void initView() {
+        mListIndustry.setAdapter(null);
+        mListRanking.setAdapter(null);
     }
 }
