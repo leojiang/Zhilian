b1340d27b87bda28f50d73f4ef73a0666ee4af95
leojiang <jiangyanghaics@163.com>
2016-02-26 18:56:48 +0800

    revised login and register UI

--- src/com/zhi/gui/guide/activity/BaseActivity.java
+++ src/com/zhi/gui/guide/activity/BaseActivity.java
@@ -3,11 +3,14 @@ package com.zhi.gui.guide.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethod;
 
 public abstract class BaseActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
     }
 }
