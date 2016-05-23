3e4eb031e5644517b90d50422e52b38cb0a05336
leojiang <jiangyanghaics@163.com>
2016-05-23 16:21:52 +0800

    添加scrollview 嵌套viewpager“

--- src/com/zhi/gui/guide/activity/FragmentFriends.java
+++ src/com/zhi/gui/guide/activity/FragmentFriends.java
@@ -1,8 +1,14 @@
 package com.zhi.gui.guide.activity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.zhi.gui.guide.R;
+import com.zhi.gui.guide.view.HomePageViewAdapter;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,12 +16,29 @@ import android.widget.TextView;
 
 public class FragmentFriends extends FragmentBase {
 
+    private ViewPager mViewPager;
+    private HomePageViewAdapter mViewPagerAdapter;
+    private List<Fragment> mFragmentList;
+
     @Override
     protected View createView(LayoutInflater inflater) {
         View root= inflater.inflate(R.layout.fragment_friends, null);
         ((TextView)root.findViewById(R.id.navigation_bar).findViewById(R.id.title)).setText("圈圈");
+
+        mViewPager = (ViewPager) root.findViewById(R.id.view_pager);
+
+        mFragmentList = new ArrayList<Fragment>();
+        mFragmentList.add(new FragmentInternship());
+        mFragmentList.add(new FragmentPersonal());
+        mViewPagerAdapter = new HomePageViewAdapter(getChildFragmentManager(), mFragmentList,
+                null);
+
+        mViewPager.setAdapter(mViewPagerAdapter);
+
+        mViewPager.setCurrentItem(0);
         return root;
     }
+
 
     @Override
     protected void onUserLogIn() {
